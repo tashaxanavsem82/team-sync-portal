759a0d653a8e320ba3455df8fc0e240c3070e5db
tashaxanavsem82 <swgwull@hotmail.com>
2025-09-24 09:08:19 -0500

    Fix typo in server log message

--- server.js
+++ server.js
@@ -12,5 +12,5 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public', 'index.html')));
 
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+    console.log(`Server is running at http://localhost:${PORT}`);
 });
