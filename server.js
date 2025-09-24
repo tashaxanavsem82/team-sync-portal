99e6efbe99176ca78fdfb004c2e32792ffd28260
tashaxanavsem82 <swgwull@hotmail.com>
2025-09-24 09:07:53 -0500

    Refactor to use arrow function for response in / route

--- server.js
+++ server.js
@@ -9,9 +9,7 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Routes
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public', 'index.html')));
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
